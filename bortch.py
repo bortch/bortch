ba3fb6af78a7e16741c81e94e892fe41fdfc4523
jbson_x240 <jb@jbson.net>
2021-11-01 18:02:19 +0100

    Using Layout

--- bortch.py
+++ bortch.py
@@ -3,28 +3,21 @@ from rich.columns import Columns
 from rich.console import Console
 from rich.panel import Panel
 from rich.tree import Tree
+from rich.layout import Layout
 
-console = Console(record=True, width=100)
+console = Console(record=True, width=None)
 
-tree = Tree("[link=https://github.com/bortch]My repositories", guide_style="bold cyan")
-ai_ml_tree = tree.add("AI & Machine Learning", guide_style="green")
-ai_ml_tree.add("[link=https://github.com/bortch/bs_lib]Own library of usefull functions")
-ai_ml_tree.add("[link=https://github.com/bortch/quickdraw]Quick attempt @ the Quick Draw Challenge")
-ai_ml_tree.add("[link=https://github.com/bortch/second_hand_UK_car_challenge]Quick attempt @ the Used UK cars challenge")
-ai_ml_tree.add("[link=https://github.com/bortch/Learning_Machine_Learning]Notes from my AI & ML Courses")
-electronics_tree = tree.add("Electronics")
-electronics_tree.add("[link=https://bortch.github.io/Upcycled-STIB-Validator/]Upcycled STIB-MIVB Validator")
+about_title = ":octopus: About me :octopus:"
 
-title = ":octopus: About me :octopus:"
-
-about = """\
+about_content = """\
 Hi there, 
 
 As I find myself difficult to describe, I would say that:
-[i] I am a little bit of everything 
-and finally almost nothing, 
-but just enough.[/]
-
+[i] 
+    A little bit of everything 
+    and finally almost nothing, 
+    but just enough.
+[/]
 I like learning, coding, experimenting, researching, modelling. 
 I feel very comfortable with Python, JavaScript and my favourite C++ and C.
 I am interested in artificial intelligence, machine learning and process modelling. 
@@ -35,11 +28,36 @@ Even if I'm mostly doubtfull about mine, I am for sharing knowledge, resources a
 Note: [i]Don't expect to find only production ready code in my repo, mainly the opposite.[/]
 """
 
-panel = Panel.fit(
-    about, box=box.SQUARE, border_style="blue", title=title, width=60
+about_panel = Panel.fit(
+    about_content, box=box.SQUARE, border_style="blue", title=about_title, width=100
 )
 
-console.print(Columns([panel, tree]))
+tree_title = "Repositories"
+
+tree = Tree("[link=https://github.com/bortch]..", guide_style="bold cyan")
+ai_ml_tree = tree.add("AI & Machine Learning", guide_style="green")
+ai_ml_tree.add("[link=https://github.com/bortch/bs_lib]Own library of usefull functions")
+ai_ml_tree.add("[link=https://github.com/bortch/quickdraw]Quick attempt @ the Quick Draw Challenge")
+ai_ml_tree.add("[link=https://github.com/bortch/second_hand_UK_car_challenge]Quick attempt @ the Used UK cars challenge")
+ai_ml_tree.add("[link=https://github.com/bortch/Learning_Machine_Learning]Notes from my AI & ML Courses")
+electronics_tree = tree.add("Electronics")
+electronics_tree.add("[link=https://bortch.github.io/Upcycled-STIB-Validator/]Upcycled STIB-MIVB Validator")
+
+tree_panel = Panel.fit(
+    tree, box=box.SQUARE, border_style="blue", title=tree_title, width=100
+)
+
+layout = Layout()
+upper = Layout(size=18)
+lower = Layout(size=10)
+layout.split_column(
+    upper,
+    lower
+)
+upper.update(about_panel)
+lower.update(tree_panel)
+
+console.print(layout)
 
 CONSOLE_HTML_FORMAT = """\
 <pre style="font-family:Menlo,'DejaVu Sans Mono',consolas,'Courier New',monospace">{code}</pre>
